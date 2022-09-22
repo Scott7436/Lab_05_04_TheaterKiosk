664db5d9069f87873dac556c3e108602e9451967
Scott7436 <gogginse@mail.uc.edu>
2022-09-22 13:22:41 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 10;
+
+        if(age >= 21)
+        {
+            System.out.println("You are " + age + " you get a wrist band");
+        }
+    }
+}
